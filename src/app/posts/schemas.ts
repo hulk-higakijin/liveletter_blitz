f52fa25d1c1eaf4bb4b5df5b640dac96596ff832
hulk-higakijin <hm385.chejptks@gmail.com>
2025-03-05 01:53:27 +0900

    feat: post scaffold

---
+++ src/app/posts/schemas.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const CreatePostSchema = z.object({
+  title: z.string().min(1),
+  emoji: z.string().length(4),
+  // template: __fieldName__: z.__zodType__(),
+});
+export const UpdatePostSchema = CreatePostSchema.merge(
+  z.object({
+    id: z.number(),
+    userId: z.undefined(),
+  })
+);
+
+export const DeletePostSchema = z.object({
+  id: z.number(),
+});
