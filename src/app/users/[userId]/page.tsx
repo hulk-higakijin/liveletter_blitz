f6c62027a28ded832337b79ebb44dc4413bb0b98
hulk-higakijin <hm385.chejptks@gmail.com>
2025-03-05 02:01:36 +0900

    feat: dynamic post in user

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next"
 import Link from "next/link"
 import { invoke } from "src/app/blitz-server"
+import getPosts from "../../posts/queries/getPosts"
 import Avatar from "../components/Avatar"
 import getUser from "../queries/getUser"
 
@@ -17,12 +18,23 @@ type UserPageProps = {
 
 export default async function Page({ params }: UserPageProps) {
   const user = await invoke(getUser, { id: params.userId })
+  const { posts } = await invoke(getPosts, { where: { userId: user.id }, orderBy: { id: "desc" } })
 
   return (
-    <div className="liveletter_container">
-      <div className="flex gap-2 pt-20">
-        <Avatar />
-        <p className="my-auto">higakijin</p>
+    <div>
+      <div className="liveletter_container">
+        <div className="flex gap-2 pt-20">
+          <Avatar />
+          <p className="my-auto">higakijin</p>
+        </div>
+      </div>
+      <div className="liveletter_container">
+        {posts.map((post) => (
+          <div key={post.id}>
+            <p>{post.title}</p>
+            <p>{post.emoji}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
