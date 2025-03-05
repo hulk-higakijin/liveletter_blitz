c0f2ee7323d2fd9ef3d9d03dcb68de5a65402dcc
hulk-higakijin <hm385.chejptks@gmail.com>
2025-03-05 15:16:22 +0900

    chore: emoji picker

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -1,14 +1,20 @@
+"use client"
+
 import PostContentEditor from "../components/PostContentEditor"
+import PostEmojiPicker from "../components/PostEmojiPicker"
 
 export default async function Page() {
   return (
     <div className="container max-w-lg mx-auto px-4">
       <div className="flex flex-col gap-2 py-10">
-        <input
-          type="text"
-          placeholder="Title"
-          className="input border-0 focus:outline-none w-full p-0 text-xl"
-        />
+        <div className="flex">
+          <input
+            type="text"
+            placeholder="Title"
+            className="input border-0 focus:outline-none w-full p-0 text-xl"
+          />
+          <PostEmojiPicker className="ml-auto my-auto" />
+        </div>
         <PostContentEditor />
       </div>
     </div>
