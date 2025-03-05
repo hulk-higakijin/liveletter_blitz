f021031b961f93e4c44693f68d7d9a5471bc5e9e
hulk-higakijin <hm385.chejptks@gmail.com>
2025-03-05 19:02:19 +0900

    feat: pen model

---
+++ db/migrations/20250305100213_create_pens/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Pen" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "content" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+
+    CONSTRAINT "Pen_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Pen" ADD CONSTRAINT "Pen_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
