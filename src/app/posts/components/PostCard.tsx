fafd09ebd5033db459fd769cb04f94358d3600ca
hulk-higakijin <hm385.chejptks@gmail.com>
2025-03-05 16:51:56 +0900

    style

--- src/app/posts/components/PostCard.tsx
+++ src/app/posts/components/PostCard.tsx
@@ -7,8 +7,8 @@ const PostCard = ({ id, title, emoji, createdAt }: Post) => {
   return (
     <Link href={`/posts/${id}`} className="card bg-base-100 shadow-sm">
       <figure className="bg-blue-100 h-40 md:h-32 rounded-lg text-3xl">{emoji}</figure>
-      <div className="card-body p-4">
-        <h2 className="text-lg">{title}</h2>
+      <div className="card-body p-2">
+        <h2>{title}</h2>
         <p className="text-sm">{format(new Date(createdAt), "yyyy/MM/dd")}</p>
       </div>
     </Link>
