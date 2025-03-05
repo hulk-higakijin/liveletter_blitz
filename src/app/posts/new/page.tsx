53b5c7512b82d21c57f15097a022e5a776152139
hulk-higakijin <hm385.chejptks@gmail.com>
2025-03-05 14:34:55 +0900

    style: input

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -1,10 +1,16 @@
-import PostContentEditor from "../components/PostContentEditor";
+import PostContentEditor from "../components/PostContentEditor"
 
 export default async function Page() {
   return (
     <div className="container max-w-lg mx-auto px-4">
-      <p>Hello</p>
-      <PostContentEditor  />
+      <div className="flex flex-col gap-2 py-10">
+        <input
+          type="text"
+          placeholder="Title"
+          className="input border-0 focus:outline-none w-full p-0 text-xl"
+        />
+        <PostContentEditor />
+      </div>
     </div>
   )
 }
