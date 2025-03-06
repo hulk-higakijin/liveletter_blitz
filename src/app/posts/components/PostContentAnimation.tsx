2cc0efeb080f117048c3583fc643bd3392927096
hulk-higakijin <hm385.chejptks@gmail.com>
2025-03-06 14:31:44 +0900

    feat: PostTitle

--- src/app/posts/components/PostContentAnimation.tsx
+++ src/app/posts/components/PostContentAnimation.tsx
@@ -6,6 +6,9 @@ import { Suspense, useEffect, useState } from "react"
 import { TypeAnimation } from "react-type-animation"
 import getPens from "../../pens/queries/getPens"
 
+const MAX_KEY_TYPING_BLANK_MILLISECONDS = 5000
+const TYPING_SPEAD = 1000
+
 const PostContentAnimation = (post: Post) => {
   return (
     <Suspense>
@@ -17,23 +20,26 @@ const PostContentAnimation = (post: Post) => {
 const Wrapper = ({ id: postId }: Post) => {
   const [currentIndex, setCurrentIndex] = useState(1)
   const [{ pens }] = useQuery(getPens, { where: { postId } })
-  console.log("pens", pens)
 
   const sequence = pens.flatMap((pen, index) => {
     const nextPen = pens[index + 1]
     const delay = nextPen
-      ? new Date(nextPen.createdAt).getTime() - new Date(pen.createdAt).getTime()
+      ? Math.min(
+          (new Date(nextPen.createdAt).getTime() - new Date(pen.createdAt).getTime()) / TYPING_SPEAD,
+          MAX_KEY_TYPING_BLANK_MILLISECONDS
+        )
       : 100
     return [pen.content, delay, () => setCurrentIndex(currentIndex + 1)]
   })
 
-  useEffect(() => {
-    console.log("currentIndex", currentIndex)
-  }, [currentIndex])
-
   return (
     <div className="whitespace-pre-wrap pt-4">
-      <TypeAnimation sequence={sequence} wrapper="span" style={{ display: "inline-block" }} />
+      <TypeAnimation
+        sequence={sequence}
+        wrapper="span"
+        style={{ display: "inline-block" }}
+        cursor={false}
+      />
     </div>
   )
 }
